a1a07da1dd6f8f3bf1e038ac287873babdde9899
GriS <emailofgris@gmail.com>
2018-08-08 23:07:38 +0400

    forms categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,8 +3,32 @@
 @section('content')
 <div class="flex1 flex">
 	<div class="flex1 flex flexWrap">
-		<div class="newCategory"></div>
-		<div class="flex1 listCategory"></div>
+		<div class="newCategory">
+			<form method="post" action="{{route('category.store')}}">
+				@csrf
+				<div class="form-group">
+					<label>Новая категория</label>
+					<input class="form-control" name="title">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-sm btn-primary">Добавить</button>
+				</div>
+			</form>
+		</div>
+		<div class="flex1 listCategory">
+			@foreach($categories as $category)
+				<div class="category">
+					<form method="post" action="{{route('category.destroy', $category->id)}}">
+						@method('DELETE')
+						@csrf
+						<input type="hidden" name="id" value="{{$category->id}}"/>
+						<label>{{$category->title}}</label>
+						<button class="btn btn-sm btn-success">Изменить</button>
+						<button class="btn btn-sm btn-danger">Удалить</button>
+					</form>
+				</div>
+			@endforeach
+		</div>
 	</div>
 	@include('categories.sidebarIndex')
 </div>
